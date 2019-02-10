f912c6aac877376360d576efaeee092e9513576b
Bruno Bocarde <brunoalvesbocarde@gmail.com>
2019-02-10 10:32:18 -0200

    Fundamentos Finalizados

--- notas/fundamentos.py
+++ notas/fundamentos.py
@@ -115,4 +115,96 @@ print(type(b))
 print(a + int(b))
 print(str(a) +b )
 
-  
+                #TIPOS NUMERICOS 
+print(dir(float))
+print(dir(int))
+print(1.1 + 2.2) 
+from decimal import Decimal, getcontext # aqui importamos a pacote decimal e getcontext para aumentar a precisão 
+print(Decimal(1) / Decimal(7))  
+getcontext().prec = 4    # mudamos a precisao p/ 4 casa decimais   
+print(Decimal(1) / Decimal(7))  
+print(Decimal.max(Decimal(1), Decimal(7))) # retorna o maior valor 
+
+
+                #TIPO STRING
+print(dir(str))
+nome = 'Saulo Pedro'
+print(nome[0]) # cada letra tem seu indice 
+print(" Texto para testar o escape d\'agua  ") #\ para texto que tenho que usar aspas 
+print("Texto com multiplas \n ..... linhas")
+nome = "ana paula"
+print(nome[0])
+print(nome[1])
+print(nome[-2]) # com o menos ele pega da direita p/ esquerda 
+print(nome[4:]) # acessa do range 4 até o ultimo
+print(nome[-5 :])
+print(nome[:3]) # do indice o até  o 2 
+numero = '1234567890'
+print(numero[::2]) # vai pulando de 2 em 2 
+print(numero[1::3]) # começa no indice 1 incluindo ele e vai pulando de 3 em 3 
+print(numero[::-1]) # mostrando a string ao contrario
+print(numero[::-2]) # mostrando ao contrario e pulando em 2 em 2
+print(nome[::-1])
+frase = 'Python é uma linguagem execelente'
+print('py'in frase) # é case sensitive 
+print('ing' in frase)
+print(len(frase))
+print(frase.lower()) # a frase foi inteira minuscula 
+print(frase.upper()) # a frase foi inteira maiuscula 
+print(frase.split('e')) # retira toda letra e 
+
+                #LISTA
+lista = []
+lista.append(1)
+lista.append(5)
+print(lista)
+nova_lista = [1,5,'ana','bia']
+nova_lista.remove(5) # o elemento 5 foi excluido nao de indice 5 
+print(nova_lista)
+nova_lista.reverse()
+print(nova_lista)
+lista = [1,5,"guilherme",'joao',3.14]
+print(lista.index('guilherme'))
+print(lista[3])
+lista = ['rui','jao','pedro','bruno']
+print(lista[1:3])
+print(lista[0:-1])
+del lista[1]
+print(lista)
+
+                #TUPLA
+tupla = ()
+tupla = tuple() # as duas maneiras estao certas 
+print(help(tuple))
+tupla = ('um')
+print(type(tupla)) 
+tupla = ('um',) # para criar um tupla de um unico elemento precisa colocar a virgula no final, pois se não vira uma str
+print(type(tupla))
+cores = ('azul','amarelo','branco','preto')
+print(cores[0])
+print(cores[-1])
+print(cores[1:])
+
+                #DICIONARIO
+pessoa = {'nome' : 'prof ana', 'idade' : 30 , 'cursos ' : ['ingles','portugues']} # mesmo comando da lista 
+
+                #CONJUNTO
+a= {1,2,3}
+print(a)
+a = set('cor3er')
+print(a) # conjunto não é indexado e nao aceita repetição 
+print({1,2,3} == {1,2,3,2,1}) # pois como sao conjuntos nao tem index e nao aceita repetição 
+#operações 
+c1 = {1,2}
+c2 = {3,2}
+print(c1.union(c2))
+print(c1.intersection(c2)) # somente ira mostara o que os 2 conjuntos tem de igual 
+c1.update(c2) # adicionou todos so elementos do c2 para c1 
+print(c1)
+c2 <= c1 # se o conjunto c2 é subconjunto de c1 
+c1 <= c2 
+print({1,2,3} - {2}) # diferença entre dois conjuntos
+
+                #INTERPOLAÇÃO 
+nome, idade = 'ana', 30.858
+print('Nome: %s Idade %.2f' %(nome,idade))
